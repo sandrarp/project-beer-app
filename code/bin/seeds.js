f4e907ba8e53e3ea26dfdf1a33f3c69189e0236e
sandrarp <sandra.rueda.pr@gmail.com>
2018-12-08 10:51:27 +0100

    Add database url

--- code/bin/seeds.js
+++ code/bin/seeds.js
@@ -10,7 +10,7 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/code', {useNewUrlParser: true})
+  .connect(`${process.env.DBURL}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
