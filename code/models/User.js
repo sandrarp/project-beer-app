d5d276d53a7428b477a8ab9f9ec9901abb7992ef
sandrarp <sandra.rueda.pr@gmail.com>
2018-12-15 10:19:07 +0100

    Edit user model

--- code/models/User.js
+++ code/models/User.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
+  name: String,
   username: String,
-  password: String
+  password: String,
+  email: String,
+  birth_date: String,
+  image: {type: String, default: 'https://res.cloudinary.com/duaxyo2ks/image/upload/v1543655128/tumblr-lab-file/profile-default.png'},
+  usertype: String,
 }, {
   timestamps: {
     createdAt: 'created_at',
